ceb5b51c9f1aa2325c398bc0cad6d2c2b8377a3f
OluwafemiPatrick <olarewajuoluwafemi34@gmail.com>
2021-09-27 14:36:21 +0100

    update banner ad id

--- lib/services/adhelper.dart
+++ lib/services/adhelper.dart
@@ -5,12 +5,12 @@ class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       String testId = 'ca-app-pub-3940256099942544/6300978111';
-      String androidLiveId = 'ca-app-pub-6758923381502381/6996428931';
+      String androidLiveId = 'ca-app-pub-8265037961520877/9110885565';
       return testId;
     }
     else if (Platform.isIOS) {
       String testId = 'ca-app-pub-3940256099942544/2934735716';
-      String iosLiveId = 'ca-app-pub-6758923381502381/5651573585';
+      String iosLiveId = 'ca-app-pub-8265037961520877/7171117930';
       return testId;
     }
     else {
